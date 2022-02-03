16f2131efe2badc213e48a9166e70795794d3cad
Gangwar <67593801+umeshkgangwarrr@users.noreply.github.com>
2022-02-03 13:14:55 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
     region = "ap-south-1"
-    access_key = "AKIA2NQ2QUK3XVIDLLHN"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
 }
